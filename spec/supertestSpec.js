1ce22f7dc38b1519f4412e7edbd79c360027ac70
György Csadó <gyorgy.csado@gmail.com>
2016-02-15 11:58:17 +0100

    add supertest

---
+++ spec/supertestSpec.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var request = require('supertest');
+
+// describe('Return users error', function () {
+//   it('supertest', function(done) {
+//     var app = server(connErr);
+//     request(app)
+//       .get('/db/users')
+//       .expect('Content-Type', /json/)
+//       .expect(500)
+//       .end(function (err, res) {
+//         if (err) throw err;
+//         done();
+//       });
+//   });
+// });
