739628b0710398a27d7c86155cfff170c2e64903
György Csadó <gyorgy.csado@gmail.com>
2016-02-16 15:54:59 +0100

    edit gulp jasmin require

--- gulpfile.js
+++ gulpfile.js
@@ -13,11 +13,3 @@ gulp.task('build', function() { console.log('Working!'); });
 gulp.task('default', function() {
   return gulpUtil.log('gulp is ready');
 });
-
-// var jasmine = require('gulp-jasmine');
-// var gulp = require('gulp');
-//
-// gulp.task('jasmine', function () {
-//   return gulp.src('./spec/*[sS]pec.js')
-//     .pipe(jasmine());
-// });
