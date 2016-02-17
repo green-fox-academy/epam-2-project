30b9502a023aa34dd16edc4942cbab5c5dd5f1e7
koda505 <koda5055@gmail.com>
2016-02-17 17:16:47 +0100

    starts registration page js

---
+++ public/js/registration.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var app = angular.module('registration', []);
+
+
+app.controller("RegistrationController", function(){
+	this.user = "";
+	this.password = "";
+
+	// this.addUser= function() {
+	// 	pass;
+	// };
+});
