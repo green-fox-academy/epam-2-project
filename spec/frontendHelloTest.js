fa6699735dd72e2413a7ce5848e04e65311817ee
Edina Frank <frankedus@gmail.com>
2016-02-18 14:16:02 +0100

    add frontend test

--- spec/frontendHelloTest.js
+++ spec/frontendHelloTest.js
@@ -1,1 +1,9 @@
 'use strict';
+
+describe('true', function () {
+  describe('saying true', function () {
+        it('should say true', function () {
+            expect(true).toBe(true);
+        });
+    });
+});
