fda27cc70c89d438634b5e8de4b45ad29cb545b7
koda505 <koda5055@gmail.com>
2016-02-19 15:18:21 +0100

    adds post function to registration

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,26 @@
 'use strict';
 
-var app = angular.module('interviewApp', ["ui.router","log"]);
-app.controller('epamController', ['$scope', "logger", function($scope, logger) {
+var app = angular.module('interviewApp', ['ui.router','log']);
+app.controller('epamController', ['$scope', 'logger', function($scope, logger) {
   logger.info('onload');
   $scope.logoname = 'Epam';
 }]);
+
+
+
+app.controller('RegistrationController', function($http) {
+  this.user = {};
+  this.addUser= function() {
+    $http.post('db/users/', this.user).then(function(response) {
+      this.okCallback = response;
+    },
+    function(err) {
+      this.errorCallback = err;
+    });
+  }
+});
+
+
 
 
 app.config(function($stateProvider, $urlRouterProvider){
@@ -13,15 +29,16 @@ app.config(function($stateProvider, $urlRouterProvider){
 
       $stateProvider
           .state('index', {
-              url: "/",
-              templateUrl: "templates/partial-main.html"
+              url: '/',
+              templateUrl: 'templates/partial-main.html'
           })
           .state('registration', {
-              url: "/register",
-              templateUrl: "templates/partial-register.html"
+              url: '/register',
+              templateUrl: 'templates/partial-register.html',
+              controller: 'RegistrationController'
           })
           .state('login', {
-              url: "/login",
-              templateUrl: "templates/partial-login.html"
+              url: '/login',
+              templateUrl: 'templates/partial-login.html'
           })
       })
