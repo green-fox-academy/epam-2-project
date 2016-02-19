b411dea6fd105f04a55f09b46aaadab8df7b624c
György Csadó <gyorgy.csado@gmail.com>
2016-02-19 11:46:37 +0100

    add sass to gulp watch

--- gulp/gulp_watch.js
+++ gulp/gulp_watch.js
@@ -3,9 +3,15 @@
 var batch = require('gulp-batch');
 var gulp = require('gulp');
 var watch = require('gulp-watch');
+var sass = require('gulp-sass');
+
+
 
 gulp.task('watch', function () {
     watch('**/*.js', batch(function (events, done) {
         gulp.start('jasmine', done);
+    }));
+    watch('**/*.scss', batch(function (events, done) {
+        gulp.start('sass', done);
     }));
 });
