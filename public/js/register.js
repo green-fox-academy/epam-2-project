a05e7163a022c9b6f041b0882a05fd7a768bf4e5
koda505 <koda5055@gmail.com>
2016-02-24 10:28:25 +0100

    adds factory for users

--- public/js/register.js
+++ public/js/register.js
@@ -2,16 +2,34 @@
 
 
 
-angular.module('register', [])
-  .controller('RegistrationController', function($http, $state) {
-    this.user = {};
-    this.addUser= function() {
-      $http.post('/api/register', this.user).then(function(response) {
-        this.okCallback = response;
-        $state.go('home');
-      },
-      function(err) {
-        this.errorCallback = err;
-      });
+var app = angular.module('register', ['log'])
+
+app.factory('userFactory', function($http) {
+    var user = {
+      email: '',
+      password: '' 
+    };
+    return {
+      user: user
     };
   });
+
+app.controller('RegistrationController', function($http, $state, logger, userFactory) {
+    this.user= {};
+    console.log(this.user);
+    console.log(userFactory.user);
+    this.addUser= function(response) {
+      $http.post('/api/register', this.user).then(function(response) {
+        console.log(response.data);
+        userFactory.email=response.data;
+        $state.go('home');
+      }),
+      function(err) {
+        logger.error(err);
+    };
+  };
+});
+
+
+
+
