003703f0e8cfd185bbe43a0c01923f384c72789e
György Csadó <gyorgy.csado@gmail.com>
2016-02-24 22:50:00 +0100

    add passport

--- server/server.js
+++ server/server.js
@@ -3,9 +3,51 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var heartbeatEndpoint = require('./heartbeat_endpoint');
 var logger = require('./logger');
-var registerEndpoint = require('./register_endpoint')
+var loginEndpoint = require('./login_endpoint');
+var registerEndpoint = require('./register_endpoint');
+var passport = require('passport');
+var Strategy = require('passport-local').Strategy;
+var users = require('./users');
 
 function setup(connection) {
+  passport.use(new Strategy({
+      usernameField: 'email',
+      passwordField: 'password',
+    },
+    function (email, password, done) {
+      users.findUser(connection, email, function (err, user) {
+        if (err) {
+          return done(err, false, 'Connection error');
+        } else if (!user) {
+          return done(null, false, 'Incorrect email');
+        } else if (user.password !== password) {
+          return done(null, false, 'Incorrect password');
+        } else {
+          return done(null, user);
+        }
+    });
+
+    passport.serializeUser(function (user, cb) {
+      cb(null, user.email);
+    });
+
+    passport.deserializeUser(function (email, done) {
+      users.findUser(connection, email, function (err, user) {
+        done(err, user);
+      });
+    });
+
+    passport.sessionLogout(function(req, res) {
+      if (!req.isAuthenticated()) {
+        res.status(500).send('Nobody logged in');
+      } else {
+        req.logout();
+        res.status(200).send('Successful logout');
+      }
+    });
+  }));
+
+
   var app = express();
   app.use(logRequest);
   app.use(express.static(__dirname +'/../public'));
@@ -13,6 +55,7 @@ function setup(connection) {
 
   app.get('/heartbeat', heartbeatEndpoint(connection));
   app.post('/api/register', registerEndpoint(connection));
+  app.post('/api/login', loginEndpoint(connection));
   app.post('/api/log', function (req, res) {
     logger[req.body.level]({
       origin: 'FRONTEND',
