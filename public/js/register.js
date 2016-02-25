8a5ccf303fd92b83924f3b3b9e74faacfb698436
Edina Frank <frankedus@gmail.com>
2016-02-25 11:47:29 +0100

    bugfix conflict

--- public/js/register.js
+++ public/js/register.js
@@ -7,12 +7,8 @@ var app = angular.module('register', ['log'])
 app.factory('userFactory', function($http) {
     var user = {
       email: '',
-<<<<<<< HEAD
-      password: ''
-=======
       password: '',
       logged: true,
->>>>>>> 12721e623e4823f15009afc78fdfd9b5e346d3ac
     };
     return {
       user: user
@@ -32,34 +28,22 @@ app.controller('RegistrationController', function($http, $state, logger, userFac
 
 
   function okCallback(response) {
-    userFactory.user.email=response.data;
+    userFactory.email=response.data;
     $state.go('home');
-    userFactory.user.logged = true;
-  }
+  };
 
   function errorCallback(err) {
     logger.error(err);
     _this.errMessage = createMessage(err);
     _this.shown = true;
-  }
+  };
 
 });
 
 function createMessage (message) {
   if (message.data.code === '23505') {
-    return 'This email already exists!';
+    return 'This email already exists!'
   } else {
-    return 'Database error. Please try again later.';
+    return 'Database error. Please try again later.'
   }
 }
-<<<<<<< HEAD
-=======
-
-app.controller('homelogging', function($scope, userFactory) {
-    $scope.email = userFactory.user.email;
-});
-
-app.controller('HomepageController', function($scope, $state, userFactory) {
-    $scope.logged = userFactory.user.logged;
-});
->>>>>>> 12721e623e4823f15009afc78fdfd9b5e346d3ac
