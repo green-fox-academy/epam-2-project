12721e623e4823f15009afc78fdfd9b5e346d3ac
György Csadó <gyorgy.csado@gmail.com>
2016-02-25 08:37:20 +0100

    add passport authentication and session

---
+++ server/login_endpoint.js
@@ -0,0 +1,35 @@
+'use strict';
+
+var logger = require('./logger');
+var passport = require('passport');
+
+
+
+
+function loginEndpoint(connection) {
+  return function (req, res, next) {
+    passport.authenticate('local', function (err, user, info) {
+      if (err) {
+        logger.console.error(err);
+        res.status(500).send(info);
+      } else if (user) {
+        req.logIn(user, function (err) {
+          if (err) {
+            return next(err);
+          }
+          logger.info(user);
+          return res.status(200).json({
+            email: user.email,
+            id: user.id,
+            status: user.status,
+          });
+        });
+      } else {
+        logger.info(info)
+        res.status(401).send(info);
+      }
+    })(req, res, next);
+  };
+}
+
+module.exports = loginEndpoint;
--- server/server.js
+++ server/server.js
@@ -10,21 +10,62 @@ var Strategy = require('passport-local').Strategy;
 var users = require('./users');
 
 function setup(connection) {
+
+
+  var app = express();
+  app.use(logRequest);
+  app.use(express.static(__dirname +'/../public'));
+  app.use(bodyParser.json());
+  app.use(passport.initialize());
+  app.use(passport.session());
+
+  app.get('/heartbeat', heartbeatEndpoint(connection));
+  app.post('/api/register', registerEndpoint(connection));
+  app.post('/api/login', function (req, res, next) {
+    passport.authenticate('local', function (err, user, info) {
+      if (err) {
+        logger.error(err);
+        res.status(500).send(info);
+      } else if (user) {
+        logger.info(user);
+        req.logIn(user, function (err) {
+          if (err) return next(err);
+          return res.status(200).json({
+            email: user.email,
+            id: user.id,
+            status: user.status,
+          });
+        });
+      } else {
+        res.status(401).send(info);
+      }
+    })(req, res, next);
+  });
+  app.post('/api/log', function (req, res) {
+    logger[req.body.level]({
+      origin: 'FRONTEND',
+      date: req.body.date,
+      level: req.body.level,
+      data: req.body.data,
+    });
+    res.status(200).json({});
+  });
+
   passport.use(new Strategy({
-      usernameField: 'email',
-      passwordField: 'password',
-    },
-    function (email, password, done) {
-      users.findUser(connection, email, function (err, user) {
-        if (err) {
-          return done(err, false, 'Connection error');
-        } else if (!user) {
-          return done(null, false, 'Incorrect email');
-        } else if (user.password !== password) {
-          return done(null, false, 'Incorrect password');
-        } else {
-          return done(null, user);
-        }
+    usernameField: 'email',
+    passwordField: 'password',
+  },
+  function (email, password, done) {
+    users(connection, email, function (err, user) {
+      if (err) {
+        return done(err, false, 'Connection error');
+      } else if (!user) {
+        return done(null, false, 'Incorrect email');
+      } else if (user.password !== password) {
+        return done(null, false, 'Incorrect password');
+      } else {
+        return done(null, user);
+      }
     });
 
     passport.serializeUser(function (user, cb) {
@@ -36,36 +77,7 @@ function setup(connection) {
         done(err, user);
       });
     });
-
-    passport.sessionLogout(function(req, res) {
-      if (!req.isAuthenticated()) {
-        res.status(500).send('Nobody logged in');
-      } else {
-        req.logout();
-        res.status(200).send('Successful logout');
-      }
-    });
   }));
-
-
-  var app = express();
-  app.use(logRequest);
-  app.use(express.static(__dirname +'/../public'));
-  app.use(bodyParser.json());
-
-  app.get('/heartbeat', heartbeatEndpoint(connection));
-  app.post('/api/register', registerEndpoint(connection));
-  app.post('/api/login', loginEndpoint(connection));
-  app.post('/api/log', function (req, res) {
-    logger[req.body.level]({
-      origin: 'FRONTEND',
-      date: req.body.date,
-      level: req.body.level,
-      data: req.body.data,
-    });
-    res.status(200).json({});
-  });
-
 
   function logRequest(req, res, next) {
     var parts = {
